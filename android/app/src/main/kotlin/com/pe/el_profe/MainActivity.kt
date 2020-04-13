7fb6f1575abddb628a8be806013b6b6f2ba0f5db
AlonsoPantigosoG <alonsopantigoso91@gmail.com>
2020-04-13 13:57:18 -0500

    -first push

---
+++ android/app/src/main/kotlin/com/pe/el_profe/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pe.el_profe
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
